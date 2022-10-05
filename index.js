73e51be258623911025d5a6b2b7da68069924382
KaosSpectre <97178572+KaosSpectre@users.noreply.github.com>
2022-10-05 10:51:41 -0400

    Prettier and actual code

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("military")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("avocado")
+favouriteFruit("tomato")
 
 // light, dark
 favouriteMode("dark")
 
 // sharp, soft, round
-favouriteEdgeStyle("soft")
+favouriteEdgeStyle("round")
 
 
 
